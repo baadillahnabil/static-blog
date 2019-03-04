ee23ae4b6760bf91153ffa93309f022b4894d3b1
baadillahnabil <nabilzero27@gmail.com>
2019-03-04 23:07:50 +0700

    add tools configs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './Layout/app'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))

f8a49c1988b44ad2716b1689c24d987f94c069b3
baadillahnabil <nabilzero27@gmail.com>
2019-03-04 23:07:41 +0700

    clean boilerplate code

---
+++ src/Layout/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import classes from './app.module.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.app}>
+        <p>Yeah</p>
+      </div>
+    )
+  }
+}
+
+export default App

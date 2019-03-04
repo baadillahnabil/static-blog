f9ef4ba217f4039e1ca656ce0f7586bea35e8b0f
baadillahnabil <nabilzero27@gmail.com>
2019-03-05 02:19:25 +0700

    add route for pages

--- src/Layout/app.jsx
+++ src/Layout/app.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import classes from './app.module.scss'
+
+// Pages
+import Users from '../pages/Users/users'
 
 class App extends Component {
   render() {
     return (
-      <div className={classes.app}>
-        <p>Yeah</p>
+      <div className={classes.rootPages}>
+        <Switch>
+          <Route path="/" exact component={Users} />
+        </Switch>
       </div>
     )
   }

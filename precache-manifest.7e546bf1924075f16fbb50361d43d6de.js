bf8e6e194a6a260519c3677b3fadd28e744fd5d7
baadillahnabil <nabilzero27@gmail.com>
2019-03-05 23:48:39 +0700

    Updates

---
+++ precache-manifest.7e546bf1924075f16fbb50361d43d6de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52f824fa0d2f8cb144a9",
+    "url": "/static-blog/static/css/main.8085106d.chunk.css"
+  },
+  {
+    "revision": "52f824fa0d2f8cb144a9",
+    "url": "/static-blog/static/js/main.52f824fa.chunk.js"
+  },
+  {
+    "revision": "c3f61d24d14c345a3d19",
+    "url": "/static-blog/static/js/runtime~main.c3f61d24.js"
+  },
+  {
+    "revision": "212b7806646b29ed4ec7",
+    "url": "/static-blog/static/js/2.212b7806.chunk.js"
+  },
+  {
+    "revision": "9a05c59df5ae3f752872395f91e0c5b8",
+    "url": "/static-blog/index.html"
+  }
+];
